63a537d20b88476f2221f47367281985c6e96152
Islomboy <i.saidabdullayev4222@gmail.com>
2022-02-05 16:45:20 +0500

    Auth is set

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use lluminate\Database\Eloquent\SoftDeletes;
 
 class Patient extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
 }
--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use lluminate\Database\Eloquent\SoftDeletes;
 
 class Treatment extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
 }
