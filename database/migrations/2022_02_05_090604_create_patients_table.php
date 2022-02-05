1520af23f7acd3110e37a5e639a9cab9be8989a7
Islomboy <i.saidabdullayev4222@gmail.com>
2022-02-05 14:49:05 +0500

    create database

---
+++ database/migrations/2022_02_05_090604_create_patients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reg_user_id')->constrained('users')
+                  ->onDelete('cascade')->onUpdate('cascade');
+            $table->string('first_name')->notNull();
+            $table->string('last_name')->notNull();
+            $table->string('middle_name')->notNull();
+            $table->date('birth_day')->notNull();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
